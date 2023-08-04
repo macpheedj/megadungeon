0cf25d9ad424c09b16b0cc5c4bfca3c85d501012
Daniel MacPhee <macpheed3@gmail.com>
2023-08-04 13:03:13 -0400

    combat movement works again

--- src/resources/MoveAction.gd
+++ src/resources/MoveAction.gd
@@ -2,7 +2,7 @@ extends Action
 class_name MoveAction
 
 
-enum { NORTH, SOUTH, EAST, WEST }
+enum Direction { North, South, East, West }
 
 
 var character: Character
@@ -15,45 +15,84 @@ var original_position: Vector2
 
 
 func on_enter(_character: Character):
-    print("[MoveAction] on_enter")
-    print("[MoveAction %s] movespeed: %s" % [_character.name, _character.movespeed])
-    character = _character
-    max_range = _character.movespeed
-    move_remaining = _character.movespeed
-    original_position = character.position
+	print("[MoveAction] on_enter")
+	print("[MoveAction %s] move: %s" % [_character.name, _character.stats.move])
+	character = _character
+	max_range = _character.stats.move
+	move_remaining = _character.stats.move
+	original_position = character.position
 
 
-func attempt_move(direction):
-    var old_position = character.position
+func is_movement_blocked(direction: Direction) -> bool:
+	var positions = {
+		Direction.North: Vector2(0, -size),
+		Direction.South: Vector2(0, size),
+		Direction.East: Vector2(size, 0),
+		Direction.West: Vector2(-size, 0),
+	}
+	character.get_node("Ray").target_position = positions[direction]
+	character.get_node("Ray").force_raycast_update()
+	return character.get_node("Ray").is_colliding()
 
-    if direction == NORTH: character.position.y -= size
-    if direction == SOUTH: character.position.y += size
-    if direction == EAST: character.position.x += size
-    if direction == WEST: character.position.x -= size
+
+func set_character_animation(direction: Direction):
+	var facing_directions = {
+		Direction.North: "up",
+		Direction.South: "down",
+		Direction.East: "right",
+		Direction.West: "left",
+	}
+	var animation = facing_directions[direction]
+	var sprite: AnimatedSprite2D = character.get_node("Sprite")
+	var frame = sprite.get_frame()
+	var progress = sprite.get_frame_progress()
+
+	sprite.play(animation)
+	sprite.set_frame_and_progress(frame, progress)
+
+
+func attempt_move(direction: Direction):
+	set_character_animation(direction)
+
+	if is_movement_blocked(direction):
+		print("movement blocked")
+		return
+
+	match direction:
+		Direction.North:
+			character.position.y -= size
+
+		Direction.South:
+			character.position.y += size
+
+		Direction.East:
+			character.position.x += size
+
+		Direction.West:
+			character.position.x -= size
 	
-    if not old_position == character.position:
-        move_remaining -= 1
-        print("spaces remaining: " + str(move_remaining))
+	move_remaining -= 1
+	print("spaces remaining: " + str(move_remaining))
 
 
 func handle_targeting():
-    if move_remaining > 0:
-        if Input.is_action_just_pressed("move_north"): attempt_move(NORTH)
-        if Input.is_action_just_pressed("move_south"): attempt_move(SOUTH)
-        if Input.is_action_just_pressed("move_east"): attempt_move(EAST)
-        if Input.is_action_just_pressed("move_west"): attempt_move(WEST)
-    
-    if Input.is_action_just_pressed("ui_accept"):
-        action_state_changed.emit(ActionComponent.State.Executing)
+	if move_remaining > 0:
+		if Input.is_action_just_pressed("move_north"): attempt_move(Direction.North)
+		if Input.is_action_just_pressed("move_south"): attempt_move(Direction.South)
+		if Input.is_action_just_pressed("move_east"): attempt_move(Direction.East)
+		if Input.is_action_just_pressed("move_west"): attempt_move(Direction.West)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		action_state_changed.emit(ActionComponent.State.Executing)
 
-    if Input.is_action_just_pressed("ui_cancel"):
-        if move_remaining < max_range:
-            character.position = original_position
-            move_remaining = max_range
-        else:
-            back_pressed.emit()
+	if Input.is_action_just_pressed("ui_cancel"):
+		if move_remaining < max_range:
+			character.position = original_position
+			move_remaining = max_range
+		else:
+			back_pressed.emit()
 
 
 func handle_execution():
-    # nothing to execute on a move action
-    action_completed.emit()
+	# nothing to execute on a move action
+	action_completed.emit()
